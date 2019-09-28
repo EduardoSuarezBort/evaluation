3615c3ec49a472e3a0a35976163e0c40207e1b82
Eduardo Suárez <eduardosuarezbort@gmail.com>
2019-09-28 00:08:15 -0400

    Solucion WebApp

---
+++ mvcEvaluation/mvcEvaluation/Models/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace mvcEvaluation.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<mvcEvaluation.Models.Student> Students { get; set; }
+
+        public System.Data.Entity.DbSet<mvcEvaluation.Models.Grade> Grades { get; set; }
+    }
+}
